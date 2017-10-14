1da20d7a7bb67919cf42b1f5923b1bd1fa19d17e
Paul Dong <paul.d@strakertranslations.com>
2017-10-15 10:09:34 +1300

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# A Facebook Messenger app for ios
+
+Following along with the online course on Youtube channel [LetsBuildThatApp](https://www.youtube.com/playlist?list=PL0dzCUj1L5JHGoEg41IJNk9QQ_hPWcyRo)
+, the code is implemented by Swift 3.2 & Xcode 9.
+
---
+++ messenger/FriendsController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  messenger
+//
+//  Created by Paul Dong on 15/10/17.
+//  Copyright © 2017 Paul Dong. All rights reserved.
+//
+
+import UIKit
+
+class FriendsController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = UIColor.red
+    }
+}
+
